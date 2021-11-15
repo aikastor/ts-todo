eb8169a9811c6d4b81d6892b00dbb6ee005b7d3d
Aikanysh Satymkulova <aikasat95@gmail.com>
2021-11-15 23:47:56 +0600

    added unique username constraint

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,12 +1,35 @@
+import {
+	ConflictException,
+	InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
+// to handle errors more gracefully:
+// define enum i.e userErrors
+// make service throw errors
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
 	async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
 		const { username, password } = authCredentialsDto;
-		const user = this.create({ username, password });
-		await this.save(user);
+
+		// hash and store
+		const salt = await bcrypt.genSalt();
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		const user = this.create({ username, password: hashedPassword });
+
+		try {
+			await this.save(user);
+		} catch (error) {
+			//duplicate username
+			if (error.code === '23505') {
+				throw new ConflictException('Username already exists');
+			} else {
+				throw new InternalServerErrorException();
+			}
+		}
 	}
 }
