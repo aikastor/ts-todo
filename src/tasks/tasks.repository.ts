720b853b7b6b085d50959b945d8028b47aa3faad
Aikanysh Satymkulova <aikasat95@gmail.com>
2021-11-18 01:08:16 +0600

    fixed pathways

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,6 +1,6 @@
 import { InternalServerErrorException, Logger } from '@nestjs/common';
 import { log } from 'console';
-import { User } from 'src/auth/user.entity';
+import { User } from '../auth/user.entity';
 import { Entity, EntityRepository, Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
